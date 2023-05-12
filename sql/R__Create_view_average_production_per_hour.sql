26c813cdd913856a8a887d9601d55325804e42f4
Michael Simons <michael@simons.ac>
2023-05-12 12:41:19 +0200

    refactor: Use proper maximum average as bar width.

--- sql/R__Create_view_average_production_per_hour.sql
+++ sql/R__Create_view_average_production_per_hour.sql
@@ -5,12 +5,14 @@ CREATE OR REPLACE VIEW average_production_per_hour AS (
         FROM production
         GROUP BY ROLLUP(Hour)
         ORDER BY Hour ASC NULLS LAST
+    ), max_Wh AS (
+        SELECT max(Wh) AS value FROM hourly_averages
     )
     SELECT hour,
            lpad(round(Wh / 1000, 2), 8, ' ') || ' ' ||
            CASE
              WHEN hour IS NULL THEN ''
-             ELSE bar(floor(Wh), 0, 10530)
+             ELSE bar(floor(Wh), 0, floor(max_Wh.value))
            END AS 'Average energy produced (kWh)'
-    FROM hourly_averages
+    FROM hourly_averages, max_Wh
 );
