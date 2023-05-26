833b7ce8adb1bdb4618c5db79b9446dd5dc51203
Michael Simons <michael@simons.ac>
2023-05-26 23:21:48 +0300

    make script more robust.

--- bin/import_production_from_kiwigrid.sh
+++ bin/import_production_from_kiwigrid.sh
@@ -17,21 +17,19 @@ curl -f --no-progress-meter "https://hems.kiwigrid.com/v2.30/analytics/productio
  -H 'Accept: application/json' \
  -H "Authorization: Bearer $BEARER" \
  -H 'Host: hems.kiwigrid.com' \
- -H 'Origin: https://new.energymanager.com' | tee \
->((echo "ts,production";  jq --raw-output '.timeseries[] | select(.name == "PowerProduced") | .values | to_entries | map("\(.key | sub("\\+0[12]:00"; ":00")),\(.value)") | .[]') > .tmp/production.csv) \
->((echo "ts,export";      jq --raw-output '.timeseries[] | select(.name == "PowerOut")      | .values | to_entries | map("\(.key | sub("\\+0[12]:00"; ":00")),\(.value)") | .[]') > .tmp/export.csv) \
-> /dev/null
+ -H 'Origin: https://new.energymanager.com' > .tmp/production.json
+
+(echo "ts,production";  jq --raw-output '.timeseries[] | select(.name == "PowerProduced") | .values | to_entries | map("\(.key | sub("\\+0[12]:00"; ":00")),\(.value)") | .[]' .tmp/production.json) > .tmp/production.csv
+(echo "ts,export";      jq --raw-output '.timeseries[] | select(.name == "PowerOut")      | .values | to_entries | map("\(.key | sub("\\+0[12]:00"; ":00")),\(.value)") | .[]' .tmp/production.json) > .tmp/export.csv
 
 curl -f --no-progress-meter "https://hems.kiwigrid.com/v2.30/analytics/consumption?type=POWER&from=${FROM}T00:00:00&to=${TO}T23:59:59&resolution=PT5M" \
  -H 'Accept: application/json' \
  -H "Authorization: Bearer $BEARER" \
  -H 'Host: hems.kiwigrid.com' \
- -H 'Origin: https://new.energymanager.com' | tee \
->((echo "ts,consumption"; jq --raw-output '.timeseries[] | select(.name == "PowerConsumed") | .values | to_entries | map("\(.key | sub("\\+0[12]:00"; ":00")),\(.value)") | .[]') > .tmp/consumption.csv) \
->((echo "ts,import";      jq --raw-output '.timeseries[] | select(.name == "PowerIn")       | .values | to_entries | map("\(.key | sub("\\+0[12]:00"; ":00")),\(.value)") | .[]') > .tmp/import.csv) \
-> /dev/null
+ -H 'Origin: https://new.energymanager.com' > .tmp/consumption.json
 
-sleep 3
+(echo "ts,consumption"; jq --raw-output '.timeseries[] | select(.name == "PowerConsumed") | .values | to_entries | map("\(.key | sub("\\+0[12]:00"; ":00")),\(.value)") | .[]' .tmp/consumption.json) > .tmp/consumption.csv
+(echo "ts,import";      jq --raw-output '.timeseries[] | select(.name == "PowerIn")       | .values | to_entries | map("\(.key | sub("\\+0[12]:00"; ":00")),\(.value)") | .[]' .tmp/consumption.json) > .tmp/import.csv
 
 xsv join ts .tmp/production.csv ts .tmp/consumption.csv  | xsv select '!ts[1]' | \
 xsv join ts /dev/stdin ts .tmp/export.csv | xsv select '!ts[1]' | \
