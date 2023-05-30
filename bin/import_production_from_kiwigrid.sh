eafbb769384195fa7ba179bff915c915ee5263f4
Michael J. Simons <michael@simons.ac>
2023-05-30 11:14:56 +0200

    This is what you get when ordering oauth flow on freaking wish.

--- bin/import_production_from_kiwigrid.sh
+++ bin/import_production_from_kiwigrid.sh
@@ -5,12 +5,26 @@ export LC_ALL=en_US.UTF-8
 
 _now=$(gdate)
 
-DB="$(pwd)/$1"
-BEARER=$2
-FROM=${3:-$(gdate -d "$_now" +'%Y-%m-%d')}
-TO=${4:-$(gdate -d "$FROM + 6 days" +'%Y-%m-%d')}
+USERNAME=$1
+PASSWORD=$2
+DB="$(pwd)/$3"
+FROM=${4:-$(gdate -d "$_now" +'%Y-%m-%d')}
+TO=${5:-$(gdate -d "$FROM + 6 days" +'%Y-%m-%d')}
 
 mkdir -p .tmp
+
+BEARER=$(
+curl -s https://auth.energymanager.com/login \
+ -H "Content-Type: application/x-www-form-urlencoded" \
+ --data "username=$USERNAME&password=$PASSWORD&autologin=false&channel=solarwatt&originalRequest=%2Fauthorize%3Fresponse_type%3Dcode%26amp%3Bredirect_uri%3Dhttps%253A%252F%252Fnew.energymanager.com%252Frest%252Fauth%252Fauth_grant%253Fchannel%253Dsolarwatt%26amp%3Bstate%3DwBdeInUYbZcqIpT15sXny6kj%26amp%3Bclient_id%3Dkiwigrid.energy-monitor-home" \
+ --cookie-jar .tmp/cookies.txt |\
+jq --raw-output '"https://auth.energymanager.com/authorize?response_type=code&state=&client_id=kiwigrid.energy-monitor-home&overrideRedirectUri=true&redirect_uri=" + .redirectUri' |\
+xargs -L 1 curl -sL --cookie .tmp/cookies.txt --cookie-jar .tmp/cookies.txt > /dev/null; \
+curl -s https://new.energymanager.com/context --cookie .tmp/cookies.txt |\
+jq --raw-output .oauth.accessToken
+)
+
+
 DIR="$(dirname "$(realpath "$0")")"
 
 curl -f --no-progress-meter "https://hems.kiwigrid.com/v2.30/analytics/production?type=POWER&splitProduction=true&from=${FROM}T00:00:00&to=${TO}T23:59:59&resolution=PT5M" \
