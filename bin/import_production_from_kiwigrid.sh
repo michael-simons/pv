93daa5a1fe75123045057fd4279aafda40a29afa
Michael Simons <michael@simons.ac>
2023-06-02 13:27:06 +0200

    refactor: Send Safari UA.

--- bin/import_production_from_kiwigrid.sh
+++ bin/import_production_from_kiwigrid.sh
@@ -12,20 +12,22 @@ PASSWORD=$3
 FROM=${4:-$(gdate -d "$_now" +'%Y-%m-%d')}
 TO=${5:-$(gdate -d "$FROM + 6 days" +'%Y-%m-%d')}
 
+UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15"
+
 mkdir -p .tmp
 
 BEARER=$(
-curl -s https://auth.energymanager.com/login \
+curl -A "$UA" -s https://auth.energymanager.com/login \
  -H "Content-Type: application/x-www-form-urlencoded" \
  --data "username=$USERNAME&password=$PASSWORD&autologin=false&channel=solarwatt&originalRequest=%2Fauthorize%3Fresponse_type%3Dcode%26amp%3Bredirect_uri%3Dhttps%253A%252F%252Fnew.energymanager.com%252Frest%252Fauth%252Fauth_grant%253Fchannel%253Dsolarwatt%26amp%3Bstate%3DwBdeInUYbZcqIpT15sXny6kj%26amp%3Bclient_id%3Dkiwigrid.energy-monitor-home" \
  --cookie-jar .tmp/cookies.txt |\
 jq --raw-output '"https://auth.energymanager.com/authorize?response_type=code&state=&client_id=kiwigrid.energy-monitor-home&overrideRedirectUri=true&redirect_uri=" + .redirectUri' |\
-xargs -L 1 curl -sL --cookie .tmp/cookies.txt --cookie-jar .tmp/cookies.txt > /dev/null; \
-curl -s https://new.energymanager.com/context --cookie .tmp/cookies.txt |\
+xargs -L 1 curl -A "$UA" -sL --cookie .tmp/cookies.txt --cookie-jar .tmp/cookies.txt > /dev/null; \
+curl -A "$UA" -s https://new.energymanager.com/context --cookie .tmp/cookies.txt |\
 jq --raw-output .oauth.accessToken
 )
 
-curl -f --no-progress-meter "https://hems.kiwigrid.com/v2.30/analytics/production?type=POWER&splitProduction=true&from=${FROM}T00:00:00&to=${TO}T23:59:59&resolution=PT5M" \
+curl -A "$UA" -f --no-progress-meter "https://hems.kiwigrid.com/v2.30/analytics/production?type=POWER&splitProduction=true&from=${FROM}T00:00:00&to=${TO}T23:59:59&resolution=PT5M" \
  -H 'Accept: application/json' \
  -H "Authorization: Bearer $BEARER" \
  -H 'Host: hems.kiwigrid.com' \
@@ -34,7 +36,7 @@ curl -f --no-progress-meter "https://hems.kiwigrid.com/v2.30/analytics/productio
 (echo "ts,production";  jq --raw-output '.timeseries[] | select(.name == "PowerProduced") | .values | to_entries | map("\(.key | sub("\\+0[12]:00"; ":00")),\(.value)") | .[]' .tmp/production.json) > .tmp/production.csv
 (echo "ts,export";      jq --raw-output '.timeseries[] | select(.name == "PowerOut")      | .values | to_entries | map("\(.key | sub("\\+0[12]:00"; ":00")),\(.value)") | .[]' .tmp/production.json) > .tmp/export.csv
 
-curl -f --no-progress-meter "https://hems.kiwigrid.com/v2.30/analytics/consumption?type=POWER&from=${FROM}T00:00:00&to=${TO}T23:59:59&resolution=PT5M" \
+curl -A "$UA" -f --no-progress-meter "https://hems.kiwigrid.com/v2.30/analytics/consumption?type=POWER&from=${FROM}T00:00:00&to=${TO}T23:59:59&resolution=PT5M" \
  -H 'Accept: application/json' \
  -H "Authorization: Bearer $BEARER" \
  -H 'Host: hems.kiwigrid.com' \
