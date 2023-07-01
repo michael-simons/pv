db51d63ca58d507de7f7f37f95fa8e9e7c749298
Michael J. Simons <michael@simons.ac>
2023-07-01 23:11:03 +0200

    refactor: Start amortization for full and partial export at same day.

--- schema/R__Create_view_amortization.sql
+++ schema/R__Create_view_amortization.sql
@@ -4,8 +4,8 @@ CREATE OR REPLACE VIEW v_amortization AS (
         FROM domain_values WHERE name = 'ACQUISITION_COST'
     ),
     beginning_of_measurements AS (
-            SELECT coalesce(time_bucket(INTERVAL '1 Month', cast(any_value(value) AS date)) + interval '1 Month', (SELECT date_trunc('month', min(measured_on)) FROM measurements)) AS value
-            FROM domain_values WHERE name = 'FIRST_PROPER_READINGS_ON'
+        SELECT coalesce(cast(any_value(value) AS date), (SELECT min(measured_on) FROM measurements)) AS value
+        FROM domain_values WHERE name = 'FIRST_PROPER_READINGS_ON'
     ),
     per_month AS (
         SELECT date_trunc('month', measured_on) AS month,
@@ -13,15 +13,16 @@ CREATE OR REPLACE VIEW v_amortization AS (
                sum(consumption) / 4 / 1000 AS consumption,
                sum(export) / 4 / 1000      AS export,
                sum(import) / 4 / 1000      AS import
-        FROM measurements
+        FROM beginning_of_measurements bom, measurements
+        WHERE date_trunc('day', measured_on) >= bom.value
         GROUP BY month
     )
     SELECT per_month.month,
            -acquisition_cost.value + round(sum(full_sell.value * per_month.production) OVER ordered_months / 100.0, 2)
                 AS full_export,
-           -acquisition_cost.value + coalesce(round(sum(part_sell.value * per_month.export + buy.gross * (per_month.production - per_month.export))
-             FILTER (WHERE date_trunc('month', per_month.month) >= bom.value) OVER ordered_months / 100.0), 0) AS partial_export
-    FROM beginning_of_measurements bom, acquisition_cost CROSS JOIN per_month
+           -acquisition_cost.value + coalesce(round(sum(part_sell.value * per_month.export + buy.gross * (per_month.production - per_month.export)) OVER ordered_months / 100.0), 0) 
+                AS partial_export
+    FROM acquisition_cost CROSS JOIN per_month
     ASOF LEFT JOIN v__selling_prices part_sell
         ON per_month.month >= part_sell.valid_from AND part_sell.type = 'partial_sell'
     ASOF LEFT JOIN v__selling_prices full_sell
