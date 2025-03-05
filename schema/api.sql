875e67567b1ee84077b66c2f9b5ffeecfc3c3396
Michael J. Simons <michael@simons.ac>
2025-03-05 11:20:49 +0100

    Improve formatter, fix `v_average_balance_per_month_and_hour`.

--- schema/api.sql
+++ schema/api.sql
@@ -529,17 +529,22 @@ CREATE OR REPLACE view v_battery_soc AS (
 -- A pivot table for average balance of export/import per month and hour
 --
 CREATE OR REPLACE VIEW v_average_balance_per_month_and_hour AS (
-  WITH production_per_month_and_hour AS (
+  WITH beginning_of_measurements AS (
+      SELECT coalesce(date_trunc('month', cast(any_value(value) AS date) + interval 1 month), (SELECT min(measured_on) FROM measurements)) AS value
+      FROM domain_values WHERE name = 'FIRST_PROPER_READINGS_ON'
+  ),
+  production_per_month_and_hour AS (
       SELECT any_value(date_part('month', measured_on))AS month,
              any_value(date_part('hour', measured_on)) AS hour,
-             avg(export-import) / 1000                 AS production
-        FROM measurements
+             avg(export-import) / 1000                 AS balance
+        FROM measurements, beginning_of_measurements bom
+       WHERE measured_on >= bom.value
        GROUP BY date_trunc('hour', measured_on)
        ORDER BY hour
   )
   PIVOT production_per_month_and_hour
   ON month IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
-  USING avg(production)
+  USING avg(balance)
   GROUP BY hour
   ORDER BY hour
 );
